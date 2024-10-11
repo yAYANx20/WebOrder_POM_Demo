4add1da74a4bd25fffa6c466a06e11891c516a07
yAYANx20 <ayanakash936@gmail.com>
2024-10-11 03:51:52 +0000

    Adding new project

---
+++ src/test/java/com/WebOrder/home/WebOrders_ViewAllProductsPage.java
@@ -0,0 +1,11 @@
+package com.WebOrder.home;
+
+import org.openqa.selenium.WebDriver;
+
+public class WebOrders_ViewAllProductsPage {
+
+	public WebOrders_ViewAllProductsPage(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/test/java/com/WebOrder/utils/WebOrders_TestData.java
@@ -0,0 +1,73 @@
+package com.WebOrder.utils;
+
+import org.testng.annotations.DataProvider;
+
+public class WebOrders_TestData {
+	@DataProvider(name = "Update Form")
+	public Object[][] UpdateOrderForm() {
+
+		return new Object[][] {
+				{ "MyMoney", "5", "Minh", "123 Main St", "Dallas", "IL", "75000", "Visa", "123456789", "12/29",
+						"success" },
+				{ "MyMoney", "5", "Nguyen", "123 Main St", "Dallas", "IL", "75000", "MasterCard", "123456789", "11/29",
+						"success" }, };
+
+	}
+
+	@DataProvider(name = "Update Order Form Error")
+	public Object[][] verifyOrderFormError() {
+
+		return new Object[][] {
+
+				{ "MyMoney", "", "Minh", "123 Main St", "Dallas", "ABC", "75000", "Visa", "123456789", "12/29",
+						"empty_quantity" },
+				{ "MyMoney", "0", "Minh", "123 Main St", "Dallas", "ILA", "75000", "Visa", "123456789", "12/29",
+						"invalid_quantity" },
+				{ "MyMoney", "5", "", "123 Main St", "Dallas", "ILA", "75000", "Visa", "123456789", "12/29",
+						"empty_name" },
+				{ "MyMoney", "5", "Minh", "", "Dallas", "IL", "75000", "Visa", "123456789", "12/29", "empty_street" },
+				{ "MyMoney", "5", "Minh", "123 Main St", "", "IL", "75000", "Visa", "123456789", "12/29",
+						"empty_city" },
+				{ "MyMoney", "5", "Minh", "123 Main St", "Dallas", "IL", "", "Visa", "123456789", "12/29",
+						"empty_zip" },
+				{ "MyMoney", "5", "Minh", "123 Main St", "Dallas", "IL", "abcde", "Visa", "123456789", "12/29",
+						"invalid_zip" },
+				{ "MyMoney", "5", "Minh", "123 Main St", "Dallas", "IL", "75000", "Visa", "", "12/29", "empty_cardnr" },
+				{ "MyMoney", "5", "Minh", "123 Main St", "Dallas", "IL", "75000", "Visa", "abcdef", "12/29",
+						"invalid_cardnr" },
+				{ "MyMoney", "5", "Minh", "123 Main St", "Dallas", "IL", "75000", "Visa", "123456789", "",
+						"empty_expdate" },
+				{ "MyMoney", "5", "Minh", "123 Main St", "Dallas", "IL", "75000", "Visa", "123456789", "abcdef",
+						"invalid_date" },
+				{ "MyMoney", "5", "Minh", "123 Main St", "Dallas", "IL", "75000", "Visa", "123456789", "12-23",
+						"invalid_date" }, };
+
+	}
+
+	@DataProvider(name = "Reset Form")
+	public Object[][] ResetForm() {
+
+		return new Object[][] {
+				{ "MyMoney", "5", "Minh", "123 Main St", "Dallas", "IL", "75000", "Visa", "123456789", "12/29",
+						"success" },
+				{ "MyMoney", "5", "Nguyen", "123 Main St", "Dallas", "IL", "75000", "MasterCard", "123456789", "11/29",
+						"success" }, };
+
+	}
+
+	@DataProvider(name = "OrdersExcelData")
+	public Object[][] OrdersDataExcel() throws Exception {
+		WebOrders_ExcelParsing excel = new WebOrders_ExcelParsing();
+		String RelativePath = System.getProperty("user.dir");
+		// Object[][] testObjArray = excel.getExcelData("C:\\Training_Scripts\\Selenium
+		// Training
+		// Data\\workspace\\Maven_Selenium_WebDriver_4\\TestDataFile\\WebOrder_Login_TestData.xls","Login");
+		Object[][] testObjArray = excel.getExcelData(RelativePath + "\\TestDataFile\\WebOrder_TestData_Login.xls",
+				"orders");
+		System.out.println(testObjArray);
+		return testObjArray;
+
+	}
+
+
+}
